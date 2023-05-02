64395b501b87653c387fb4ab6f8f6a333bf42a7b
bleronmorina <bleronmorina54@gmail.com>
2023-05-02 23:27:20 +0200

    Few fixes

--- Project/src/main/java/Controllers/LoginController.java
+++ Project/src/main/java/Controllers/LoginController.java
@@ -3,10 +3,16 @@ package Controllers;
 import Models.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import Services.UserAuthService;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class LoginController {
@@ -14,9 +20,9 @@ public class LoginController {
     private TextField txtEmail;
     @FXML
     private PasswordField pwdPassword;
-
+    boolean isLoginSuccessful = false;
     @FXML
-    private void loginClick(ActionEvent e){
+    private void loginClick(ActionEvent e) throws IOException {
         String email = this.txtEmail.getText();
         String password = this.pwdPassword.getText();
         try{
@@ -26,9 +32,24 @@ public class LoginController {
                 System.out.println("Username or password is incorrect!");
                 return;
             }
+            else {
+                isLoginSuccessful = true;
+            }
             System.out.println("User is correct!");
         }catch (SQLException sqlException) {
 
+        }
+        if (isLoginSuccessful) {
+            // Create a new scene for the home page
+            Parent homePageParent = FXMLLoader.load(getClass().getResource("/Main/Home.fxml"));
+            Scene homePageScene = new Scene(homePageParent);
+
+            // Get the stage information
+            Stage window = (Stage) ((Node) e.getSource()).getScene().getWindow();
+
+            // Set the new scene on the stage
+            window.setScene(homePageScene);
+            window.show();
         }
     }
     @FXML
--- Project/src/main/java/Main/Main.java
+++ Project/src/main/java/Main/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(
-                Main.class.getResource("login.fxml")
+                Main.class.getResource("Login.fxml")
         );
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Lower Education Managment System");
